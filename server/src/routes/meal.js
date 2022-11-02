6aaeae04c76971a24a75a278c181d244f8e8e3ae
Ibrahim Akancay <iakancay@gmail.com>
2022-11-02 01:54:12 +0100

    crud operations and meal querries added

--- server/src/routes/meal.js
+++ server/src/routes/meal.js
@@ -1,9 +1,22 @@
 import express from "express";
-import { createMeal, getMeals } from "../controllers/meal.js";
+import {
+  createMeal,
+  getMeals,
+  searchMeals,
+  filterMeals,
+  updateMeal,
+  deleteMeal,
+  getMeal,
+} from "../controllers/meal.js";
 
 const mealRouter = express.Router();
 
 mealRouter.get("/", getMeals);
 mealRouter.post("/create", createMeal);
+mealRouter.patch("/:id", updateMeal);
+mealRouter.get("/:id", getMeal);
+mealRouter.delete("/:id", deleteMeal);
+mealRouter.get("/query", searchMeals); // meals/query?search=beef
+mealRouter.get("/filter", filterMeals); // meals/filter?cuisine=Turkish&category=Vegan
 
 export default mealRouter;

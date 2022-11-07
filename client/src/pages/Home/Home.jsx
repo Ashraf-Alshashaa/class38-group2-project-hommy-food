450a4102b6c31af8e293f524a169e1e73f60e131
Ibrahim Akancay <iakancay@gmail.com>
2022-11-07 14:46:34 +0100

    upload widget added Home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import TEST_ID from "./Home.testid";
 import SearchField from "../../components/SearchField";
@@ -6,27 +6,25 @@ import CuisineListButtons from "../../components/CuisineListButtons/index";
 import CategoryListCards from "../../components/CategoryListCards/index";
 import Banner from "../../components/Banner";
 import "./style.css";
-import UploadImgWidget from "../../components/UploadImgWidget";
+//import UploadImgWidget from "../../components/UploadImgWidget";
 
 const Home = () => {
-  const [imgUrl, setImgUrl] = useState("");
-  console.log(imgUrl);
+  //const [imgUrl, setImgUrl] = useState("");
+  //console.log(imgUrl);
   return (
     <>
-    <div data-testid={TEST_ID.container}>
-      <div className="home-main-container">
-        <Banner />
-        <div className="home-search-container">
-          <SearchField />
-          <CuisineListButtons />
-          <CategoryListCards />
+      <div data-testid={TEST_ID.container}>
+        <div className="home-main-container">
+          <Banner />
+          <div className="home-search-container">
+            <SearchField />
+            <CuisineListButtons />
+            <CategoryListCards />
+          </div>
         </div>
-        
       </div>
-      
-    </div>
-   
-    <UploadImgWidget setImgUrl={setImgUrl} />
+
+      {/* <UploadImgWidget setImgUrl={setImgUrl} /> */}
     </>
   );
 };

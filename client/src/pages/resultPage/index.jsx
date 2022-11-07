12613ef3e08d9582730b82a3b568af0f1acb4a6e
cynthia Amanat <cynthiasusana.ary@gmail.com>
2022-11-07 23:57:09 +0100

    add loding img

--- client/src/pages/resultPage/index.jsx
+++ client/src/pages/resultPage/index.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import CategoryCard from "../../components/CategoryListCards/CategoryCard";
 import useFetch from "../../hooks/useFetch";
 import "./index.css";
+import sadChef from "./sadChef.jpg";
+import loader from "./orange_circles.gif";
 export default function ResultPage() {
   const [searchParams] = useSearchParams();
   const [data, setData] = useState([]);
@@ -11,17 +13,19 @@ export default function ResultPage() {
   const cuisine = searchParams.get("cuisine");
   const {
     performFetch: performSearchFetch,
-    isLoading,
-    error,
+    isLoading: isLoadingSearch,
+    error: errorSearch,
   } = useFetch(`/meals/search?query=${query}`, setData);
-  const { performFetch: performFilterCategoryFetch } = useFetch(
-    `/meals/filter?category=${category}`,
-    setData
-  );
-  const { performFetch: performFilterCuisineFetch } = useFetch(
-    `/meals/filter?cuisine=${cuisine}`,
-    setData
-  );
+  const {
+    performFetch: performFilterCategoryFetch,
+    isLoading: isLoadingCategory,
+    error: errorCategory,
+  } = useFetch(`/meals/filter?category=${category}`, setData);
+  const {
+    performFetch: performFilterCuisineFetch,
+    isLoading: isLoadingCuisine,
+    error: errorCuisine,
+  } = useFetch(`/meals/filter?cuisine=${cuisine}`, setData);
 
   useEffect(() => {
     query && performSearchFetch();
@@ -29,17 +33,28 @@ export default function ResultPage() {
     cuisine && performFilterCuisineFetch();
   }, []);
 
-  return isLoading ? (
+  return isLoadingSearch || isLoadingCuisine || isLoadingCategory ? (
     <>
-      <h2>Loading...</h2>
+      <img className="loading-gif" src={loader} alt="loader" />
     </>
-  ) : error ? (
+  ) : errorSearch || errorCategory || errorCuisine ? (
     <>
-      <h2>{error}</h2>
+      <h5>
+        {errorSearch.message || errorCategory.message || errorCuisine.message}
+      </h5>
     </>
   ) : (
     <>
       <div className="result-page">
+        {!data?.result?.length > 0 && (
+          <div className="meals-notFound">
+            <img src={sadChef} alt="sad chef" />
+            <h1>Oops!</h1>
+            <h5>
+              Sorry no meals found go back to <Link to="/">HomePage</Link>
+            </h5>
+          </div>
+        )}
         {data?.result?.map((meal) => (
           <CategoryCard key={meal._id} image={meal.image} title={meal.title} />
         ))}

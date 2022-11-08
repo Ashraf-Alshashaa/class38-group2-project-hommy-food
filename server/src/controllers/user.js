0f93f52eb9aeeca411dd5d26a11894e5ac6363ee
Ashraf-Alshashaa <ashraf.alchechae@gmail.com>
2022-11-08 12:40:18 +0100

    changes for tring new deployment

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -66,7 +66,7 @@ export const authenticateToken = async (req, res, next) => {
   const token = authHeader && authHeader.split(" ")[1];
   if (token === null) return res.sendStatus(401);
   try {
-    const user = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     req.user = user;
     next();
   } catch (error) {

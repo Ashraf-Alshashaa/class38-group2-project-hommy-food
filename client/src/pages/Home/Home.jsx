9250ece66e6b07a08dc5792741339fb1c77cb5e5
Fadi <fady-naddaf@hotmail.com>
2022-11-08 01:41:57 +0100

    add div in home page and make the header sticky

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,13 +4,16 @@ import SearchField from "../../components/SearchField";
 import CuisineListButtons from "../../components/CuisineListButtons/index";
 import CategoryListCards from "../../components/CategoryListCards/index";
 import Banner from "../../components/Banner";
+import "./style.css";
 
 const Home = () => {
   return (
     <div data-testid={TEST_ID.container}>
       <div className="home-main-container">
         <Banner />
-        <SearchField />
+        <div className="home-search-container">
+          <SearchField />
+        </div>
         <CuisineListButtons />
         <CategoryListCards />
       </div>

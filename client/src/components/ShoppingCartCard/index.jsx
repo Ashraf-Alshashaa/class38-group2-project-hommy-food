69f3d94cbe084439ccabd69cb9bbbd328a004382
cynthia Amanat <cynthiasusana.ary@gmail.com>
2022-11-17 21:52:46 +0100

    creating shopping cart list cards

---
+++ client/src/components/ShoppingCartCard/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import imageforStyle from "./jbq2pdytmaodjawkr1pe.jpg";
+import "./style.css";
+const ShoppingCartCard = ({ image, title, quantity, price }) => {
+  return (
+    <div className="product">
+      <div className="product-image">
+        <img src={imageforStyle} />
+      </div>
+      <div className="product-title">meat and vegs</div>
+      <div className="product-price">12.99</div>
+      <div className="product-quantity">
+        <button className="shopping-cart-btn">+</button>
+        <input type="text" />
+        <button className="shopping-cart-btn">-</button>
+      </div>
+      <div className="product-removal">
+        <button className="remove-product">Remove</button>
+      </div>
+      <div className="product-line-price">25.98</div>
+    </div>
+  );
+};
+ShoppingCartCard.PropTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string,
+  quantity: PropTypes.number,
+  price: PropTypes.number,
+  id: PropTypes.string,
+};
+export default ShoppingCartCard;
--- client/src/pages/ShoppingCart/index.jsx
+++ client/src/pages/ShoppingCart/index.jsx
@@ -3,6 +3,7 @@ import { AuthContext } from "../../contexts/authentication";
 import { Link } from "react-router-dom";
 import emptyShoppingCartImage from "./shoppingCart.gif";
 import "./style.css";
+import ShoppingCartCard from "../../components/ShoppingCartCard";
 const ShoppingCartPage = () => {
   const { user } = useContext(AuthContext);
   console.log(user);
@@ -10,7 +11,7 @@ const ShoppingCartPage = () => {
   return (
     <>
       <div className="shopping-cart-page-container">
-        {user?.cart.length === 0 && (
+        {/* {user?.cart.length === 0 && (
           <div className="shopping-cart-page-empty">
             <img src={emptyShoppingCartImage} alt="empty shopping cart" />
             <h1>Shopping cart is empty</h1>
@@ -18,7 +19,30 @@ const ShoppingCartPage = () => {
               please add meals from <Link to="/"> HomePage</Link>
             </h4>
           </div>
-        )}
+        )} */}
+        {/* header */}
+        <header className="header-shopping-cart-container">
+          <h1>Shopping Cart</h1>
+
+          <div className="breadcrumb">
+            <Link to="/">HomePage</Link>
+          </div>
+
+          <span className="count">items in the cart</span>
+        </header>
+        <div className="column-labels">
+          <label className="shopping-cart-page-image">Image</label>
+          <label className="shopping-cart-page-details">Product</label>
+          <label className="shopping-cart-page-price">Price</label>
+          <label className="shopping-cart-page-quantity">Quantity</label>
+          <label className="shopping-cart-page-removal">Remove</label>
+          <label className="shopping-cart-page-line-price">Total</label>
+        </div>
+        {user?.cart.map((meal) => (
+          <ShoppingCartCard key={meal._id} />
+        ))}
+        <ShoppingCartCard />
+        <ShoppingCartCard />
       </div>
     </>
   );

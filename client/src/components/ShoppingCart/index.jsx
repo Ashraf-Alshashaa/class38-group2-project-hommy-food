28b0bd05539dbffb022b0795629d73ceae1dd370
Fadi <fady-naddaf@hotmail.com>
2022-11-18 12:32:20 +0100

    Adding more condition for Shopping cart & fix the input form style

--- client/src/components/ShoppingCart/index.jsx
+++ client/src/components/ShoppingCart/index.jsx
@@ -4,39 +4,56 @@ import PropTypes from "prop-types";
 import { AuthContext } from "../../contexts/authentication";
 import ShoppingCartPopUp from "../ShoppingCartPopUp";
 import "./style.css";
+import { MsgPopupContext } from "../../contexts/msgPopup";
 
 const ShoppingCart = ({ id, chefId }) => {
   const { user, setUser } = useContext(AuthContext);
+  const { setPopup } = useContext(MsgPopupContext);
   const [closeModal, setCloseModal] = useState(false);
   const [msg, setMsg] = useState("");
   const navigate = useNavigate();
 
-  const chefIdInCart = user?.cart[0]?.mealId?.chefId;
-
   const handleCartClick = async () => {
+    const chefIdInCart = user?.cart[0]?.mealId?.chefId;
+    const findMeal = user?.cart?.filter((meal) => meal.mealId._id === id);
+    const totalQuantity = findMeal[0]?.mealId?.quantity;
+    const orderedQuantity = findMeal[0]?.quantity;
     if (chefId === user?._id) {
       return;
     }
     if (!chefIdInCart || chefId === chefIdInCart) {
-      const token = localStorage.getItem("accessToken");
-      try {
-        const response = await fetch(
-          `${process.env.BASE_SERVER_URL}/api/customer/shopping-cart/add-to-cart/${id}`,
-          {
-            method: "PATCH",
-            headers: {
-              "content-type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        const data = await response.json();
-        setUser(data.result);
-      } catch (error) {
-        window.alert("sorry something went wrong");
+      if (!orderedQuantity || totalQuantity > orderedQuantity) {
+        const token = localStorage.getItem("accessToken");
+        try {
+          const response = await fetch(
+            `${process.env.BASE_SERVER_URL}/api/customer/shopping-cart/add-to-cart/${id}`,
+            {
+              method: "PATCH",
+              headers: {
+                "content-type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          const data = await response.json();
+          setUser(data.result);
+          setPopup({
+            type: "success",
+            text: "The meal added to cart",
+            open: true,
+          });
+        } catch (error) {
+          window.alert("sorry something went wrong");
+        }
+      } else {
+        setPopup({
+          type: "error",
+          text: "No more available quantity",
+          open: true,
+        });
       }
     } else {
-      return setCloseModal(true);
+      setCloseModal(true);
     }
   };
 

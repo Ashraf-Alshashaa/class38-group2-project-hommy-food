f1a343c023c171aff28d25354e7adbd31b77ba11
Osama Bazbooz <obazbooz@gmail.com>
2022-11-18 13:34:46 +0100

    Fix the switching between customer and chef profile

--- client/src/pages/ProfilePage/index.jsx
+++ client/src/pages/ProfilePage/index.jsx
@@ -21,7 +21,7 @@ const ProfilePage = () => {
   }, []);
   return (
     <>
-      {user?.isChef || !user ? (
+      {user?._id !== id || !user ? (
         <div className="chef-profile-page">
           <ProfileHeader chefData={chefData} setChefData={setChefData} />
           <PersonalInfo id={id} chefData={chefData} setChefData={setChefData} />

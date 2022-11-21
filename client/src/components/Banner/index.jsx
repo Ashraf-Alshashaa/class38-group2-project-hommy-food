38d11d2ec885a94d5fd847c716f6572058c92843
Fadi <fady-naddaf@hotmail.com>
2022-11-21 23:09:12 +0100

    Changing the slogan and make it resonsive

--- client/src/components/Banner/index.jsx
+++ client/src/components/Banner/index.jsx
@@ -10,10 +10,9 @@ const Banner = () => {
         <img src={banner} alt="banner img" className="banner-img" />
         <img src={dishes} alt="banner img" className="dishes-img" />
         <div className="slogan-section">
-          <h3 className="slogan">Do not</h3>
-          <h3 className="slogan-eat">Eat</h3>
-          <h3 className="slogan-light">light</h3>
-          <h3 className="slogan-tasty">tasty</h3>
+          <h3 className="slogan-top">Food that</h3>
+          <h3 className="slogan-middle">reminds you</h3>
+          <h3 className="slogan-bottom">of your home</h3>
         </div>
       </div>
     </div>

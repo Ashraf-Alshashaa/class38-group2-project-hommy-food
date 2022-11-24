a978dd4c35e29768a86ac3d73f353be5aabad6de
cynthia Amanat <cynthiasusana.ary@gmail.com>
2022-11-24 16:27:26 +0100

    final check

--- client/src/components/OurTeamCard/teamData.js
+++ client/src/components/OurTeamCard/teamData.js
@@ -3,14 +3,10 @@ const teamMembers = [
     id: 1,
     name: "Ashraf Alshashaa",
     image: "https://ca.slack-edge.com/T0EJTUQ87-U03BGTV9SRH-ff4ba9bf6ebc-512",
-    gitHubLink: "",
+    gitHubLink: " https://github.com/Ashraf-Alshashaa",
     TwitterLink: "",
-    LinkedinLink: "",
-    InstagramLink: "",
-    motivation: ` I am a motivated person seeking employment that can help me grow as a
-        professional developer and make use of my existing skills. I am a team
-        player who is always ready to learn and support the team. Inspired by
-        the technological advancements and willing to contribute for future.`,
+    LinkedinLink: "https://www.linkedin.com/in/ashraf-alshashaa/",
+    InstagramLink: " https://www.facebook.com/ashraf13795",
   },
   {
     id: 2,
@@ -20,23 +16,15 @@ const teamMembers = [
     TwitterLink: "",
     LinkedinLink: "https://www.linkedin.com/in/ibrahim-akancay-3817b1b2/",
     InstagramLink: "",
-    motivation: ` I am a motivated person seeking employment that can help me grow as a
-        professional developer and make use of my existing skills. I am a team
-        player who is always ready to learn and support the team. Inspired by
-        the technological advancements and willing to contribute for future.`,
   },
   {
     id: 3,
     name: "Cynthia Amanat",
-    image: "http://localhost:8080/e0e4b925ef574f53932e7931296e1d0c.png",
+    image: "https://ca.slack-edge.com/T0EJTUQ87-U03BK87F7QU-8d695192f468-512",
     gitHubLink: "https://bit.ly/cynthiaAmant",
     TwitterLink: "",
     LinkedinLink: "https://bit.ly/393m36I",
     InstagramLink: "",
-    motivation: ` I am a motivated person seeking employment that can help me grow as a
-        professional developer and make use of my existing skills. I am a team
-        player who is always ready to learn and support the team. Inspired by
-        the technological advancements and willing to contribute for future.`,
   },
   {
     id: 4,
@@ -46,10 +34,6 @@ const teamMembers = [
     TwitterLink: "",
     LinkedinLink: "https://www.linkedin.com/in/fadi-naddaf-a04ba7196/",
     InstagramLink: "",
-    motivation: ` I am a motivated person seeking employment that can help me grow as a
-        professional developer and make use of my existing skills. I am a team
-        player who is always ready to learn and support the team. Inspired by
-        the technological advancements and willing to contribute for future.`,
   },
   {
     id: 5,
@@ -59,10 +43,6 @@ const teamMembers = [
     TwitterLink: "",
     LinkedinLink: "",
     InstagramLink: "",
-    motivation: ` I am a motivated person seeking employment that can help me grow as a
-        professional developer and make use of my existing skills. I am a team
-        player who is always ready to learn and support the team. Inspired by
-        the technological advancements and willing to contribute for future.`,
   },
 ];
 export default teamMembers;
